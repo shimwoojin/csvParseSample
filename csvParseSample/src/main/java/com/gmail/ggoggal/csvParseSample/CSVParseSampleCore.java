1acfa0ae0404f53dd22afe2411049e8bd3d16c9f
woojin <woojin@DESKTOP-PHTDJLD>
2021-03-18 13:42:11 +0900

    first commit

---
+++ csvParseSample/src/main/java/com/gmail/ggoggal/csvParseSample/CSVParseSampleCore.java
@@ -0,0 +1,15 @@
+package com.gmail.ggoggal.csvParseSample;
+
+
+import java.util.stream.Collectors;
+
+public class CSVParseSampleCore
+{
+	public static void main(String[] args) throws Exception
+	{
+		EQDataLoader loader = new EQDataLoader();
+		loader.storeData(CSVParseSampleCore.class.getResourceAsStream("/sample.csv"));
+		String result = loader.getData().stream().map(d -> d.toString()).collect(Collectors.joining("\n"));
+		System.out.println(result);
+	}
+}
